162323d726480bb2b6372462f035d3b86416bcbe
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-02-03 14:48:58 +0530

    Add project files.

---
+++ Location_Tracker/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Location_Tracker
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver driver = new ChromeDriver();
+            string Url = "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwiO1sjrrs3uAhXkzzgGHSpxD2EQPAgI";
+
+            IWebElement image = driver.FindElement(By.CssSelector("#hplogo"));
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine(image.Location.X);
+            Console.WriteLine(image.Size.Height);
+            Console.WriteLine(image.Size.Width);
+            Console.WriteLine(image.Location.Y);
+        }
+    }
+}
